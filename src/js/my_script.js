f82c8c8895572ad004c9d3b97b5fa42c7354d03a
Grégory Sandoz <gregory.sandoz@rpn.ch>
2017-05-18 16:23:30 +0200

    last commit of the day

--- src/js/my_script.js
+++ src/js/my_script.js
@@ -20,11 +20,12 @@ function add() {
     document.getElementById("SelectClients").appendChild(newOption);
     newOption.innerHTML = nomComplet;
 }
-
-function del() {
+function listToText() {
     "use strict";
     /*Variable declaration*/
     var dropListValue = document.getElementById("SelectClients").value,
+        table = document.getElementById("ClientsTable"),
+        cells = table.getElementsByTagName("td"),
         str = dropListValue,
         array = str.split(" "), /*The string gets splitted into two parts, these parts each go in a different cell*/
         a = array[0],
@@ -38,6 +39,15 @@ function del() {
             document.getElementById("InputName").value = a;
             document.getElementById("InputFirstName").value = b;   
         }
-    /*Deletes the row*/
-    
+}
+function del() {
+    "use strict";
+    var table = document.getElementById("ClientsTable"),
+        tabTr = table.getElementsByTagName("tr"),
+        trLength = tabTr.length,
+        tabTd = table.getElementsByTagName("td"),
+        tdLenght = tabTd.length,
+        InputName = document.getElementById("InputName1").value,
+        InputFirstName = document.getElementById("InputFirstName1").value;
+    alert("td : " + tdLenght + " " + "tr : " +trLength);
 }
