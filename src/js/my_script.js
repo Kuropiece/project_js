3fba6b132902ed70914ff758ff0544bc9adcef1c
Grégory Sandoz <gregory.sandoz@rpn.ch>
2017-06-01 10:30:02 +0200

    Finished del function, added anylisis pictures and report file

--- src/js/my_script.js
+++ src/js/my_script.js
@@ -8,13 +8,16 @@ function add() {
         td2 = document.createElement("td"),
         nomComplet = InputName + " " + InputFirstName,
         newOption = document.createElement("option");
+    document.getElementById("InputFirstName1").value = "";
+    document.getElementById("InputName1").value = "";
     
     /*Adding input values into the table*/
-    if(InputName === "" || InputFirstName === ""){ /*Avoids adding empty values to the table*/
-        alert("Veuillez entrer un nom correct.")
+    if(InputName === "" || InputFirstName === "") { /*Avoids adding empty values to the table*/
+        alert("Veuillez entrer un nom correct.");
         return;
-    }else{
+    } else {
         document.getElementById("ClientsTable").appendChild(row);
+        row.setAttribute("id", InputName+InputFirstName);
         row.appendChild(td1);
         row.appendChild(td2);
         td1.innerHTML = InputName;
@@ -25,6 +28,7 @@ function add() {
     document.getElementById("SelectClients").appendChild(newOption);
     newOption.innerHTML = nomComplet;
 }
+/*Adds the client's name from the droplist into the text inputs*/
 function listToText() {
     "use strict";
     /*Variable declaration*/
@@ -36,37 +40,26 @@ function listToText() {
         a = array[0],
         b = array[1];
     /*Puts split names into textboxes*/
-    if (a === "")
-        {
-            document.getElementById("InputName").value = ""; /*Prevents "InputFirstName" from reading "Undefined" when the list goes back to blank*/
-            document.getElementById("InputFirstName").value = "";
+    if (a === "") {
+            document.getElementById("DelName").value = ""; /*Prevents "InputFirstName" from reading "Undefined" when the list goes back to blank*/
+            document.getElementById("DelFirstName").value = "";
         } else {
-            document.getElementById("InputName").value = a;
-            document.getElementById("InputFirstName").value = b;   
+            document.getElementById("DelName").value = a;
+            document.getElementById("DelFirstName").value = b;  
         }
 }
+/*Deletes the chosen client (selected in the droplist) from the table, the list, and the text inputs*/
 function del() {
     "use strict";
     /*Variable declaration*/
-    var table = document.getElementById("ClientsTable"),
-        tabTr = table.getElementsByTagName("tr"),
-        trLength = tabTr.length,
-        tabTd = table.getElementsByTagName("td"),
-        tdLenght = tabTd.length,
-        InputName = document.getElementById("InputName1").value,
-        InputFirstName = document.getElementById("InputFirstName1").value;
-
-    /*Browsing through the html table to find matching values*/
-    for (var i=0; i<tabTr.length; i++){
-        alert("1");
-        for (var iTd=0; iTd<tabTd.length; iTd++){
-            alert("2");
-            if(tabTd[iTd] === InputName){
-                alert("3");
-                if(tabTd[iTd+1] === InputFirstName){
-                    alert("4");
-                }
-            }
-        }
-    }
+    var name = document.getElementById("DelName").value,
+        firstName = document.getElementById("DelFirstName").value,
+        fullName = name + firstName,
+        element = document.getElementById(fullName),
+        list = document.getElementById("SelectClients");
+    element.parentNode.removeChild(element);
+    list.remove(list.selectedIndex);
+    document.getElementById("DelName").value = "";
+    document.getElementById("DelFirstName").value = "";
+    
 }
